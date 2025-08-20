7a165b4790fd629cde1f9a4c350f944827ce72d2
Omar <elmanssouriomar@gmail.com>
2025-08-20 12:27:11 +0100

    add Tag entity

---
+++ src/main/java/com/omardev/blog/domain/entities/Tag.java
@@ -0,0 +1,37 @@
+package com.omardev.blog.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    // Explicit equals & hashCode to avoid Lombok issues with JPA entities
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tag tag)) return false;
+        return id != null && id.equals(tag.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}

a0ac0e0b29e6659303482a42945a7efac7c0438a
Omar <elmanssouriomar@gmail.com>
2025-08-22 01:44:00 +0100

    add persistence layer

---
+++ src/main/java/com/omardev/blog/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.omardev.blog.repositories;
+
+
+import com.omardev.blog.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
---
+++ src/main/java/com/omardev/blog/repositories/TagRepository.java
@@ -0,0 +1,11 @@
+package com.omardev.blog.repositories;
+
+import com.omardev.blog.domain.entities.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+}
